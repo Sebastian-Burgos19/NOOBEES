5ecc0c57a2e7c822402fb45709e4dce282600716
Sebastian Burgos Barrera <104393774+Sebastian-Burgos19@users.noreply.github.com>
2023-05-15 10:51:39 -0500

    v3.1
    v3.1

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
 import HelloWorld from './components/HelloWorld.vue'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import HoInfo from './components/HoInfo.vue';
 </script>
 
 <template>
   <NavBar />
+    <HoInfo />
+    <h1>hola</h1>
 
   <Footer />
  
---
+++ src/components/HoInfo.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <img src="" alt="NOOBEES">
+        <button class="button btn">Empezemos</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
